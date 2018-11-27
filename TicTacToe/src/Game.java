73fa3b58e1c78f17298dedaeee6b6ea3f5cd7762
jon <43426006+jcwalp@users.noreply.github.com>
2018-11-27 11:46:55 -0500

    Update

--- TicTacToe/src/Game.java
+++ TicTacToe/src/Game.java
@@ -5,60 +5,61 @@ public class Game {
 	public static final char empty = '-';
 	public static final char pl1 = 'x';
 	public static final char pl2 = 'o';
-	
-	//constants for board 
+
+	//constants for board
 	public static final int rows = 3, columns = 3;
 	public static final char[][] board = new char[rows][columns];
+	public static final char[][] positions = new char[rows][columns];
+	public static final char[][] WinPlayer = {{'x','x','x'},
+											 {'x','x','x'},
+										     {'x','x','x'}};
+	public static final char[][] WinComputer ={{'o','o','o'},
+											   {'o','o','o'},
+											   {'o','o','o'}};
 	//setting scanner up
 	public static final Scanner kb = new Scanner(System.in);
-	
-	public static boolean isPlayersTurn;
+	public static boolean isPlaying = true;
+	public static boolean isPlayersTurn = true;
+	public static boolean isComputersTurn = false;
+	public static boolean WIN = false;
+	public static boolean LOSE = false;
+	public static boolean DRAW = false;
 	public static void main (String[] args) {
 		initializeGame();
-		while (!gameOver()) {
-			printGameState();
-			isPlayersTurn = true;
-			System.out.println("X pos: ");
-			int positionX = kb.nextInt();
-			System.out.println("Y pos: ");
-			int positionY = kb.nextInt();
-			insertPlayerMove(positionX, positionY);
-			isPlayersTurn = false;
-			randomPlayerMove();
-		}
+
+
 	}
-	
+
 	public static void initializeGame() {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				board[i][j] = empty;
+				positions[i][j] = empty;
 			}
 		}
 	}
-	
+
 	public static void printGameState() {
 		System.out.println(board[0]);
 		System.out.println(board[1]);
 		System.out.println(board[2]);
 	}
-	
-	public static boolean gameOver() {
-		if (board[0][1] == 'x') {
-			return false;
-		}
-		else {
-			return false;
-		}
+
+	public static void updatePlayerGame(int PlayerX, int PlayerY) {
+		board[PlayerX][PlayerY] = pl1;
+		positions[PlayerX][PlayerY] = pl1;
 	}
-	
-	public static void insertPlayerMove(int positionX, int positionY) {
-		board[positionX-1][positionY-1] = pl1;
+
+	public static void updateComputerGame() {
+
 	}
-	
-	public static void randomPlayerMove() {
-		if (!isPlayersTurn) {
-			for (int count = 0; count != 1; count++) {
-				
+
+	public static void checkForWin() {
+		//row check (x x x)
+		for (int i = 0; i < rows; i++) {
+			if (positions[i] == WinPlayer[0]) {
+				WIN = true;
 			}
 		}
+	}
 }
